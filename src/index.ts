4f07bb7a9ff835614847da0776109e0ed5058b67
robinyeon <robin.yeon@gmail.com>
2022-10-25 18:43:22 +0900

    FEAT: MySQL 연결

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,35 @@
-import express from 'express';
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
+import mysql from "mysql";
+import dbconfig from "./config/connection";
+const connection = mysql.createConnection(dbconfig);
+
+connection.connect();
+
+connection.query("SELECT * FROM test", (error, rows, fields) => {
+  if (error) throw error;
+  console.log("Test info: ", rows);
+});
+
+connection.end();
 
 // 라우터 불러오기
-import {testRouter} from './routes';
+import { testRouter } from "./routes";
 
 class App {
-    app: express.Application;
+  app: express.Application;
 
-    constructor() {
-        this.app = express();
-    }
+  constructor() {
+    this.app = express();
+  }
 }
 
 dotenv.config();
 
 const app = new App().app;
 
-app.get('/', testRouter);
+app.get("/", testRouter);
 
 app.listen(process.env.PORT, () => {
-    console.log('http://localhost:3002');
-});​
+  console.log("http://localhost:3002");
+});
