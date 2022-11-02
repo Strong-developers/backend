7000903b1fcd0d02ce795f882006204fbf2a3cf0
HyunSeong <hhs950120@gmail.com>
2022-11-02 21:43:38 +0900

    FEAT: 서브 카테고리 API 삭제

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -2,13 +2,18 @@ import { MainCategory, SubCategory } from "../models";
 import ApiError from "../utils/ApiError";
 
 export default {
+  /**
+   * 메인 카테고리 불러오기
+   * 
+   * @returns 
+   */
   async findMainCategoryList() {
     const mainCategory = await MainCategory.findAll({
       attributes: ["id", "name", "path"],
       raw: true,
     });
 
-    const result = await Promise.all(
+    await Promise.all(
       mainCategory.map(async (el) => {
         const subCategory = await SubCategory.findAll({
           attributes: ["id", "parent_id", "name", "path"],
@@ -22,22 +27,5 @@ export default {
     );
 
     return mainCategory;
-  },
-
-  async findSubCategoryList(id) {
-    const subCategory = await SubCategory.findAll({
-      attributes: ["id", "parent_id", "name", "path"],
-      where: {
-        parent_id: id,
-      },
-    });
-
-    if (!subCategory.length) {
-      throw ApiError.setBadRequest(
-        "ID에 해당하는 서브 카테고리가 존재하지 않습니다."
-      );
-    }
-
-    return subCategory;
   },
 };
