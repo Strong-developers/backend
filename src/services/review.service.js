da3251bd46fdb8b18ce4fa92db2f4f4d7faeb864
HyunSeong <hhs950120@gmail.com>
2022-11-04 01:56:11 +0900

    FIX: 준호님께 '지적' 받았던 코드 수정 - 상수 따로 관리하는 utils/Constant.js 생성 - 댓글 총 페이지 계산할 때 즉시 실행 함수 사용

--- src/services/review.service.js
+++ src/services/review.service.js
@@ -1,46 +1,44 @@
 import { ReviewComment } from "../models";
+import { PER_PAGE } from "../utils/Constant";
 
 export default {
   /**
    * 게시물에 달린 댓글의 총 개수 리턴
-   * 
-   * @param {number} postId 
-   * @returns 
+   *
+   * @param {number} postId
+   * @returns
    */
   async selectCommentPageCount(postId) {
-    let totalCommentPage = null;
-    const perPage = 10;
     const commentCount = await ReviewComment.count({
       where: {
-        postId
-      }
+        postId,
+      },
     });
 
-    if (commentCount % perPage === 0) {
-      totalCommentPage = commentCount / perPage;
-    } else {
-      totalCommentPage = Math.floor(commentCount / perPage) + 1;
-    }
-
-    return totalCommentPage;
+    return (() => {
+      if (commentCount % PER_PAGE === 0) {
+        return commentCount / PER_PAGE;
+      } else {
+        return Math.floor(commentCount / PER_PAGE) + 1;
+      }
+    })();
   },
 
   /**
    * 게시물에 해당하는 댓글을 10개 단위로 불러옴
-   * 
-   * @param {number} postId 
-   * @param {number} page 
-   * @returns 
+   *
+   * @param {number} postId
+   * @param {number} page
+   * @returns
    */
   async selectComment(postId, page) {
-    const perPage = 10;
-    const {commentList} = await ReviewComment.findAll({
-      attributes: ['id', 'owner_id', 'post_id', 'comment'],
+    const { commentList } = await ReviewComment.findAll({
+      attributes: ["id", "owner_id", "post_id", "comment"],
       where: {
         postId,
       },
-      offset: (page - 1) * perPage,
-      limit: perPage,
+      offset: (page - 1) * PER_PAGE,
+      limit: PER_PAGE,
     });
 
     return commentList;
@@ -48,10 +46,10 @@ export default {
 
   /**
    * 댓글 생성하는 서비스
-   * 
-   * @param {number} postId 
-   * @param {number} ownerId 
-   * @param {string} comment 
+   *
+   * @param {number} postId
+   * @param {number} ownerId
+   * @param {string} comment
    */
   async insertComment(postId, ownerId, comment) {
     await ReviewComment.create({
@@ -63,9 +61,9 @@ export default {
 
   /**
    * 댓글 수정하는 서비스
-   * 
-   * @param {number} commentId 
-   * @param {string} comment 
+   *
+   * @param {number} commentId
+   * @param {string} comment
    */
   async updateComment(commentId, comment) {
     await ReviewComment.update(
@@ -80,8 +78,8 @@ export default {
 
   /**
    * 댓글 삭제하는 서비스
-   * 
-   * @param {number} commentId 
+   *
+   * @param {number} commentId
    */
   async deleteComment(commentId) {
     await ReviewComment.destroy({
---
+++ src/utils/Constant.js
@@ -0,0 +1,4 @@
+export default {
+  // 후기 게시판에서 보여주는 댓글 개수
+  PER_PAGE: 10,
+}
