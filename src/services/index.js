05f2d61082b6452f1625a78406412bbd24cea053
robinyeon <robin.yeon@gmail.com>
2022-11-03 23:33:49 +0900

    FIX: service index.js 충돌해결

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,6 @@
 import categoryService from "./category.service";
-<<<<<<< HEAD
 import boardService from "./board.service";
 import authService from "./auth.service";
-
-export { categoryService, boardService, authService };
-=======
 import reviewService from "./review.service";
 
-export { categoryService, reviewService };
->>>>>>> 6cd9ae21d27e373853c62a49839511d61a3f7c3c
+export { categoryService, boardService, authService, reviewService };
