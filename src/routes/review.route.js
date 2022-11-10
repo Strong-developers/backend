763a3160bbb41e9dd237deb78998816922eb8eda
HyunSeong <hhs950120@gmail.com>
2022-11-11 01:43:37 +0900

    FIX: 후기 게시판 댓글 API 엔드포인트 변경 - comments/ -> posts/comments

--- src/routes/review.route.js
+++ src/routes/review.route.js
@@ -9,9 +9,9 @@ router.post("/posts", verifyToken, reviewCtrl.addReview);
 router.put("/posts/:postId",verifyToken, reviewCtrl.modifyReview);
 router.delete("/posts/:postId",verifyToken, reviewCtrl.removeReview);
 
-router.get("/comments/:postId", reviewCtrl.getCommentList);
-router.post("/comments/:postId",verifyToken, reviewCtrl.addComment);
-router.put("/comments/:commentId",verifyToken, reviewCtrl.modifyComment);
-router.delete("/comments/:commentId",verifyToken, reviewCtrl.removeComment);
+router.get("/posts/comments/:postId", reviewCtrl.getCommentList);
+router.post("/posts/comments/:postId",verifyToken, reviewCtrl.addComment);
+router.put("/posts/comments/:commentId",verifyToken, reviewCtrl.modifyComment);
+router.delete("/posts/comments/:commentId",verifyToken, reviewCtrl.removeComment);
 
 export default router;
